46847027191e6000e9bd30144e9f2a9096d850aa
arvidn <arvid@libtorrent.org>
2022-05-26 14:04:55 +0200

    extend benchmark to make the db-sync mode and node-profiler configurable

--- tools/run_benchmark.sh
+++ tools/run_benchmark.sh
@@ -2,13 +2,15 @@
 
 run_benchmark() {
    # shellcheck disable=SC2086
-   python ./tools/test_full_sync.py run $3 --profile --test-constants "$1" &
+   python ./tools/test_full_sync.py run $3 --profile --node-profiler --test-constants "$1" &
    test_pid=$!
    python ./tools/cpu_utilization.py $test_pid
    mkdir -p "$2"
    mv test-full-sync.log cpu.png cpu-usage.log plot-cpu.gnuplot "$2"
    python ./tools/test_full_sync.py analyze
    mv slow-batch-*.profile slow-batch-*.png "$2"
+   python ./chia/util/profiler.py profile-node >"$2/node-profile.txt"
+   mv profile-node "$2"
 }
 
 cd ..
